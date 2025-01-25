0b936f17abfa00a9e78bcef7a171a38f374f9442
Relict <vostryakovkirill@gmail.com>
2025-01-25 20:15:54 +0400

    обновление

--- src/test/java/ru/netology/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/CashBackHackerServiceTest.java
@@ -3,17 +3,17 @@ package ru.netology.service;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CashbackHackerServiceTest {
+public class CashBackHackerServiceTest {
     @Test
     public void testRemainWhenAmountIs900() {
-        CashBackHackerService service = new CashBackHackerService();
+        ru.netology.service.CashBackHackerService service = new ru.netology.service.CashBackHackerService();
         int result = service.remain(900);
         Assert.assertEquals(result, 100);
     }
 
     @Test
     public void testRemainWhenAmountIs1000() {
-        CashBackHackerService service = new CashBackHackerService();
+        ru.netology.service.CashBackHackerService service = new ru.netology.service.CashBackHackerService();
         int result = service.remain(1000);
         Assert.assertEquals(result, 0);
     }
